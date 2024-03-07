9342ee7f7861e75eb516248ff5e6ee25681a4897
gabriel Cabrera <gabriel.cabera@gmail.com>
2024-03-07 14:51:18 -0400

    initialize the theme

---
+++ acf-blocks.php
@@ -0,0 +1,10 @@
+<?php
+
+//ACF Blocks
+add_action('init', 'register_acf_blocks');
+
+function register_acf_blocks()
+{
+    register_block_type(__DIR__ . '/blocks/hero');
+   
+}
--- functions.php
+++ functions.php
@@ -1,5 +1,16 @@
 <?php 
 define('theme_version', time());
+function dd($data = '')
+{
+    if ($data != '') {
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    } else {
+        echo "No Text";
+    }
+}
+
 
 // Adding theme styles and scripts
 add_action('wp_enqueue_scripts', 'af_add_theme_scripts');
@@ -24,4 +35,39 @@ function af_add_theme_scripts() {
         'tailwind',
         get_template_directory_uri() . '/dist/output.css',
     );
+}
+
+
+//Register ACF blocks
+include_once('acf-blocks.php');
+
+add_theme_support('post-thumbnails');
+
+
+//Add Menu 
+function menu()
+{
+    register_nav_menus(array(
+        'primary' => 'Primary Navigation',
+        'an-extra-menu' => 'An Extra Menu',
+    ));
+}
+
+add_action('after_setup_theme', 'menu');
+
+
+//Register general settings page
+if (function_exists('acf_add_options_page')) {
+
+    if (function_exists('acf_add_options_page')) {
+
+        acf_add_options_page(array(
+            'page_title'    => 'Theme General Settings',
+            'menu_title'    => 'Theme Settings',
+            'menu_slug'     => 'theme-general-settings',
+            'capability'    => 'edit_posts',
+            'position' => '3.5',
+            'redirect'      => false
+        ));
+    }
 }
